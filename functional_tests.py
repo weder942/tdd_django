2db7997bf453abdbe2af044556d90b78433d6411
weder <wedercarlos20@hotmail.com>
2022-05-23 21:48:47 -0300

    Front page HTML now generated from a template"

--- functional_tests.py
+++ functional_tests.py
@@ -41,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)			
 
 		# Ainda contina havendo uma caixa de texto convidado-a a acrescentar
