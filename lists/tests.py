d5b1f09ec49a390d96701167cea4522fa80d6cd4
weder <wedercarlos20@hotmail.com>
2023-03-18 18:18:26 -0300

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
