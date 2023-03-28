3533b21b68345688e191d7cbe36498ed9dca696c
weder <wedercarlos20@hotmail.com>
2023-03-28 19:00:15 -0300

    Refactoring somethings

--- lists/tests.py
+++ lists/tests.py
@@ -90,4 +90,20 @@ class ListViewTest(TestCase):
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+<<<<<<< HEAD
 >>>>>>> a6fc08a (New URL, view and template to display lists)
+=======
+
+class NewListTest(TestCase):
+
+	def test_can_save_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+	
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+>>>>>>> 391a0bd (Refactoring somethings)
