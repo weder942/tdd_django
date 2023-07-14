9c8dbc5d7cf34b69d6305860680a71ed22e7f3a5
weder <wedercarlos20@hotmail.com>
2023-07-13 21:28:09 -0300

    First version FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,21 +1,37 @@
+from selenium.webdriver.common.keys import Keys
 from functional_tests.base import FunctionalTest
-from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
-	@skip
 	def test_cannot_add_empty_list_items(self):
 		# Edith acessa a página inicial e acidentalmente tenta submeter
 		# um item vazio na lista. Ela tecla Enter na caixa de entrada vazia
+		self.browser.get(self.live_server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
 		# A página inicial é atualizada e há uma mensagem de erro informando
 		# que itens da lista não podem estar em branco
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element_by_css_selector('.has-error').text,
+			"You can't have an empty list item"
+		))
 
 		# Ela tenta novamente com um texto para o item, e isso agora funciona
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
 
 		# De forma perversa, ela agora decide submeter um segundo item
 		# Branco
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
 		# Ela recebe um aviso semelhante na página da lista
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element_by_css_selector('.has-error').text,
+			"You can't have an empty list item"
+		))
 
 		# E ela pode corrir isso preenchendo o item com um texto
-		self.fail('write me!')
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+		self.wait_for_row_in_list_table('2: Make tea')
