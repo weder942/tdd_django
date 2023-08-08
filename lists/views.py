4912ae834a833f63831736fa6b04390ec8320ca9
weder <wedercarlos20@hotmail.com>
2023-08-08 14:18:28 -0300

    Refatoracao concluida. Nao removi o codigo abaixo dos returns para nao dificultar muito minha vida no acompanhamento do livro

--- lists/views.py
+++ lists/views.py
@@ -7,11 +7,29 @@ def home_page(request):
     
     return render(request, 'home.html')
 
+def add_new_item(request, list_, new_list):
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+        if not new_list:
+            return redirect(f'/lists/{list_.id}/')
+    except ValidationError:
+        error = "You can't have an empty list item"
+        if new_list:
+            list_.delete()
+            return render(request, 'home.html', {'error': error})
+            
+    if new_list:
+        return redirect('view_list', list_.id)
+    
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 def view_list(request, list_id):
     error = ''
     list_ = List.objects.get(id=list_id)
     if request.method == 'POST':
+        return add_new_item(request, list_, False)
         item = Item(text=request.POST['item_text'], list=list_)
         try:
             item.full_clean()
@@ -26,6 +44,8 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
+
+    return add_new_item(request, list_, True)
     item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
